618db902f3c61c0b975a8d5b344311b8b0cd9bbc
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-04-25 18:04:01 +0800

    Update routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Testnet from './components/Testnet/Testnet'
 
@@ -8,10 +8,10 @@ function App() {
   return (
       <Router basename={process.env.PUBLIC_URL}>
         <div className="app h-100" style={{"backgroundColor": "rgb(244, 246, 248)"}}>
-          <Routes>
-            <Route path='/' exact element={<Home/>}/>
-            <Route path="/testnet" exact element={<Testnet/>} />
-          </Routes>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/testnet" component={Testnet}></Route>
+          </Switch>
         </div>
       </Router>
   );
